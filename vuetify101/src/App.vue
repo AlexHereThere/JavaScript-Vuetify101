691e20c819f40e25b764b4c60c3df3862847f511
Alejandro Castro <a1183297@uabc.edu.mx>
2024-02-24 18:59:42 -0800

    Primera version Vuetify101

---
+++ vuetify101/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-layout class="rounded rounded-md">
+    <v-app-bar title="Application bar"></v-app-bar>
+
+  <v-navigation-drawer>
+    <v-list-item title="Cosas que hacer" subtitle="y mas..."></v-list-item>
+    <v-divider></v-divider>
+    <v-list-item  title="Home" link to="/"></v-list-item>
+    <v-list-item  title="Diversion" link to="/diversionP"></v-list-item>
+    <v-list-item  title="To-Do" link to="/listaP"></v-list-item>
+  </v-navigation-drawer>
+
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+      <router-view/>
+    </v-main>
+  </v-layout>
+</template>
+
+<script setup>
+  //
+</script>
---
+++ vuetify101/src/pages/diversionP.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-card
+      class="mx-auto"
+      max-width="300"
+      position='relative'
+    >
+      <v-img
+        class="align-end text-white"
+        height="300"
+        :src="urlImagen"
+        cover
+      >
+        <v-card-title>Mi Imagen Aleatoria</v-card-title>
+      </v-img>
+  
+      <v-card-subtitle class="pt-4">
+        Mi Chiste Aleatorio
+      </v-card-subtitle>
+  
+      <v-card-text>
+        <div>{{chisteTexto}}</div>
+      </v-card-text>
+  
+      <v-card-actions>
+  
+        <v-btn color="red" elevation="12" block="true" @click=cambiarElementos>
+          Cambiar
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+</template>
+
+    <script setup>
+    import {ref} from "vue";
+    let urlImagen = ref();
+    let chisteTexto;
+    cambiarElementos();
+    async function cambiarElementos()
+    {
+      let respuesta = await fetch("https://picsum.photos/200/300");
+      let broma = await fetch("https://api.chucknorris.io/jokes/random")
+      let datos = await broma.json();
+      chisteTexto = datos.value;
+      urlImagen.value = respuesta.url;
+    }
+    </script>
