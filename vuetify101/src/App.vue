801a0db4e059553e4ce54429b2da55322434b910
Alejandro Castro <a1183297@uabc.edu.mx>
2024-03-06 23:20:20 -0800

    Mejoras a Navegation Drawer, implementacion para Meta 2.2

--- vuetify101/src/App.vue
+++ vuetify101/src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <v-layout class="rounded rounded-md">
-    <v-app-bar title="Application bar"></v-app-bar>
+    <v-app-bar title="MegaTarea"></v-app-bar>
 
-  <v-navigation-drawer>
-    <v-list-item title="Cosas que hacer" subtitle="y mas..."></v-list-item>
+  <v-navigation-drawer  expand-on-hover rail>
+    <v-list-item prepend-icon="mdi-account-cowboy-hat" title="Cosas que hacer" subtitle="y mas..."></v-list-item>
     <v-divider></v-divider>
-    <v-list-item  title="Home" link to="/"></v-list-item>
-    <v-list-item  title="Diversion" link to="/diversionP"></v-list-item>
-    <v-list-item  title="To-Do" link to="/listaP"></v-list-item>
+    <v-list-item prepend-icon="mdi-home" title="Home" link to="/"></v-list-item>
+    <v-list-item prepend-icon="mdi-mushroom" title="Diversion" link to="/diversionP"></v-list-item>
+    <v-list-item prepend-icon="mdi-list-box" title="To-Do" link to="/listaP"></v-list-item>
+    <v-list-item prepend-icon="mdi-comment-text-multiple-outline" title="Comentarios" link to="/comP"></v-list-item>
   </v-navigation-drawer>
 
     <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
@@ -17,5 +18,5 @@
 </template>
 
 <script setup>
-  //
+//
 </script>
